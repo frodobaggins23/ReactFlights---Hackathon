5d920ad2cda5814528f01757a54117d1ed63734e
seannimmo <Sean@Seans-MacBook-Pro.local>
2019-07-05 11:18:19 +0200

    fetch finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from "react";
-import "./App.css";
-import Navbar from "./components/Navbar.js";
-import FlightResultsTable from "./components/FlightResultsTable.js";
-import FlightFinder from "./components/FlightFinder.js";
+import React, { Component } from "react"
+import "./App.css"
+import Navbar from "./components/Navbar.js"
+import FlightResultsTable from "./components/FlightResultsTable.js"
+import FlightFinder from "./components/FlightFinder.js"
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <FlightFinder />
-      <FlightResultsTable />
-    </div>
-  );
+class App extends Component {
+  state = {
+    flightResults: []
+  }
+  componentDidMount() {
+    const flightUrl = new URL("https://api.skypicker.com/flights?")
+    const params = {
+      flyFrom: "PRG",
+      to: "VLC",
+      dateFrom: "06/07/2019",
+      dateTo: "07/07/2019",
+      partner: "picky",
+      limit: "5"
+    }
+    flightUrl.search = new URLSearchParams(params)
+    console.log("flightUrl", flightUrl)
+    fetch(flightUrl)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ flightResults: data.data })
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <FlightFinder />
+        <FlightResultsTable results={this.state.flightResults} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
