06b12abde6082673720ff6f28d082cd2860c3bf5
seannimmo <Sean@Seans-MacBook-Pro.local>
2019-07-05 13:55:27 +0200

    search feature finished

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import FlightFinder from "./components/FlightFinder.js"
 
 class App extends Component {
   state = {
-    flightResults: []
-  }
-  componentDidMount() {
-    const flightUrl = new URL("https://api.skypicker.com/flights?")
-    const params = {
+    flightResults: [],
+    isLoading: false,
+    params: {
       flyFrom: "PRG",
       to: "VLC",
       dateFrom: "06/07/2019",
@@ -18,21 +16,55 @@ class App extends Component {
       partner: "picky",
       limit: "5"
     }
+  }
+  componentDidMount() {
+    this.fetchFlights()
+  }
+
+  fetchFlights = () => {
+    this.setState({ isLoading: true })
+
+    const flightUrl = new URL("https://api.skypicker.com/flights?")
+    const { params } = this.state
+
     flightUrl.search = new URLSearchParams(params)
-    console.log("flightUrl", flightUrl)
     fetch(flightUrl)
       .then(response => response.json())
       .then(data => {
-        this.setState({ flightResults: data.data })
+        this.setState({ isLoading: false, flightResults: data.data })
       })
+  }
+
+  changeSource = e => {
+    const { target } = e
+
+    this.setState(prevState => ({
+      params: { ...prevState.params, flyFrom: target.value }
+    }))
+  }
+
+  changeDestination = e => {
+    const { target } = e
+
+    this.setState(prevState => ({
+      params: { ...prevState.params, to: target.value }
+    }))
   }
 
   render() {
     return (
       <div className="App">
         <Navbar />
-        <FlightFinder />
-        <FlightResultsTable results={this.state.flightResults} />
+        <FlightFinder
+          searchFunction={this.fetchFlights}
+          changeSource={this.changeSource}
+          changeDestination={this.changeDestination}
+        />
+        {this.state.isLoading ? (
+          <div>Loading</div>
+        ) : (
+          <FlightResultsTable results={this.state.flightResults} />
+        )}
       </div>
     )
   }
--- src/components/FlightFinder.js
+++ src/components/FlightFinder.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React from "react"
 
-export default function FlightFinder() {
-  return <div>This is the finder</div>;
+export default function FlightFinder(props) {
+  return (
+    <>
+      <h2>Origin</h2>
+      <select onChange={props.changeSource} name="departure">
+        <option value="PRG">Prague</option>
+        <option value="BER">Berlin</option>
+        <option value="WAW">Warsaw</option>
+        <option value="PED">Pardubice</option>
+      </select>
+      <h2>Destination</h2>
+      <select onChange={props.changeDestination} name="destination">
+        <option value="VLC">Valencia</option>
+        <option value="BCN">Barcelona</option>
+        <option value="MAD">Madrid</option>
+        <option value="LIN">Milano</option>
+        <option value="ATH">Athens</option>
+      </select>
+
+      <input onClick={props.searchFunction} type="submit" value="Search" />
+    </>
+  )
 }
