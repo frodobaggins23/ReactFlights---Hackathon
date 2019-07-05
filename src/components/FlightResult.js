4deaeb78a49cc0ec732411ae1c4be47113fb223f
frodobaggins23 <46576318+frodobaggins23@users.noreply.github.com>
2019-07-05 12:32:40 +0200

    flights table completed

--- src/components/FlightResult.js
+++ src/components/FlightResult.js
@@ -1,5 +1,30 @@
 import React from "react";
+import { DateTime } from "luxon";
 
-export default function FlightResult() {
-  return <div>This is one result</div>;
-}
+const FlightResult = ({
+  departure,
+  arrival,
+  dTime,
+  aTime,
+  price,
+  flightNo
+}) => {
+  return (
+    <div>
+      <h4>Flight no: {flightNo + 1}</h4>
+      <div>From:{departure}</div>
+      <div>To:{arrival}</div>
+      <div>
+        Departs at:
+        {DateTime.fromMillis(dTime * 1000).toFormat("dd-LL-yyyy hh:mm")}
+      </div>
+      <div>
+        Arrives at:
+        {DateTime.fromMillis(aTime * 1000).toFormat("dd-LL-yyyy hh:mm")}
+      </div>
+      <div>Price:{price}</div>
+    </div>
+  );
+};
+
+export default FlightResult;
--- src/components/FlightResultsTable.js
+++ src/components/FlightResultsTable.js
@@ -1,11 +1,21 @@
 import React from "react";
 import FlightResult from "./FlightResult.js";
 
-export default function FlightResultsTable() {
+export default function FlightResultsTable({ results }) {
   return (
     <div>
       <h1>This is the Flight table </h1>
-      <FlightResult />
+      {results.map((result, index) => (
+        <FlightResult
+          departure={result.cityFrom}
+          arrival={result.cityTo}
+          dTime={result.dTime}
+          aTime={result.aTime}
+          price={result.price}
+          key={index}
+          flightNo={index}
+        />
+      ))}
     </div>
   );
 }
